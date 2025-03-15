8a876caf5caa0224066edb01c63d2fcb04ed3dff
Kion <kion@dashgl.com>
2025-03-15 06:12:48 +0000

    added poc for collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,35 +6,7 @@ const cardCollection = defineCollection({
   schema: z.object({
     name: z.string(),
     username: z.string(),
-    profilePic: z.string(),
-    profileBanner: z.string(),
-    followers: z.number(),
-    following: z.number(),
-    isBlueCheck: z.boolean(),
-    weakness: z.object({
-      amount: z.number(),
-      type: z.string()
-    }),
-    resists: z.object({
-      amount: z.number(),
-      type: z.string()
-    }),
-    createdOn: z.string().transform(str => new Date(str)),
-    rarity: z.string().optional(),
-    hp: z.string().optional(),
-    abilities: z.array(z.object({
-      name: z.string(),
-      type: z.string(),
-      description: z.string()
-    })),
-    attacks: z.array(z.object({
-      name: z.string(),
-      type: z.string(),
-      damage: z.number(),
-      chance: z.string(),
-      description: z.string()
-    })),
-    title: z.string().optional()
+    title: z.string()
   })
 });
 
